bf1dab19011921a500559d71ee1086bbcfe74aa9
tamirz <tamir.zur@gmail.com>
2018-04-21 13:16:33 +0300

    creating ActorSystem for the StreamProcessingApp

--- src/main/scala/io/bigpanda/streamprocessing/StreamProcessingApp.scala
+++ src/main/scala/io/bigpanda/streamprocessing/StreamProcessingApp.scala
@@ -1,6 +1,16 @@
 package io.bigpanda.streamprocessing
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.scalalogging.LazyLogging
+import io.bigpanda.streamprocessing.api.CounterRoutes
+
 object StreamProcessingApp extends App with LazyLogging {
 
+  private implicit val system: ActorSystem = ActorSystem("stream-processing-app")
+  private implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  private val config: Config = ConfigFactory.load()
 
 }
