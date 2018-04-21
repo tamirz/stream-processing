b8ecb553f8ccba2f5753d6f45d7a2ca655d7b553
tamirz <tamir.zur@gmail.com>
2018-04-22 01:09:38 +0300

    added server.request.timeout to configuration

--- src/main/scala/io/bigpanda/streamprocessing/service/CounterService.scala
+++ src/main/scala/io/bigpanda/streamprocessing/service/CounterService.scala
@@ -1,18 +1,17 @@
 package io.bigpanda.streamprocessing.service
 
 import java.util.concurrent.TimeUnit
-
 import akka.actor.{ActorRef, ActorSystem}
 import com.typesafe.scalalogging.LazyLogging
 import io.bigpanda.streamprocessing.repository.CounterRepository._
 import akka.pattern.ask
 import akka.util.Timeout
-
+import com.typesafe.config.Config
 import scala.concurrent.Future
 
-class CounterService(implicit system: ActorSystem, implicit val counterRepository: ActorRef) extends LazyLogging {
+class CounterService(config: Config)(implicit system: ActorSystem, implicit val counterRepository: ActorRef) extends LazyLogging {
 
-  implicit lazy val timeout: Timeout = Timeout(5, TimeUnit.SECONDS)
+  implicit lazy val timeout: Timeout = Timeout(config.getInt("server.request.timeout"), TimeUnit.SECONDS)
 
   def updateWordCounter(word: String): Unit = {
     logger.debug(s"Updating word counter, for word: $word")
